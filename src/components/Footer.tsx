cd89c0cb962e8b2852e22abdc94b710829631f1b
svaraborut <borutsvara@gmail.com>
2023-11-07 21:03:54 +0100

    feat: better footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
-import React, { useMemo } from 'react';
-import { Container } from '@comp/Container.tsx';
-import { HOSTNAME, HOSTNAME_ALL } from '../conf.ts';
-import { SocialLinks } from '@comp/SocialLinks.tsx';
+import React, {useMemo} from 'react';
+import {Container} from '@comp/Container.tsx';
+import {HOSTNAME, HOSTNAME_ALL} from '../conf.ts';
+import {SocialLinks} from '@comp/SocialLinks.tsx';
+import {SpanFlash} from "@comp/SpanFlash.tsx";
+import {version} from '../../package.json'
 
 export function Footer() {
 
@@ -28,9 +30,20 @@ export function Footer() {
             {/*    </p>*/}
             {/*</div>*/}
 
+            <div className='text-md text-gray-500'>
+                <p>
+                    Interested in how this simple website is done?&nbsp;
+                    <SpanFlash>
+                        <a href='https://github.com/svaraborut/website' target='_blank'>
+                            Check out the repository
+                        </a>
+                    </SpanFlash>
+                </p>
+            </div>
+
             <div className='col-span-full flex justify-between text-gray-500'>
                 <p>© Copyright {year.toFixed(0)}</p>
-                <p>{HOSTNAME_ALL.map(x => x.name).join(', ')}</p>
+                <p>v{version} - {HOSTNAME_ALL.map(x => x.name).join(', ')}</p>
             </div>
 
         </Container>
